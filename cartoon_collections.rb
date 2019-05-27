d3a1ca0345e716ff7cbd8ee0b6c245e98a5967d5
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-27 20:13:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,19 +1,21 @@
 def roll_call_dwarves(names)
- names.each_with_index {|n, i| print "#{i+1}. #{n}"}
+  names.each_with_index {|n, index| print "#{index +1}. #{n}"}
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect {|calls| calls.capitalize + "!"}
+#planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+
+def summon_captain_planet(calls)
+  calls.collect {|c| c.capitalize + "!"}
 end
 
 def long_planeteer_calls(calls)
-  calls.any? {|c| c.length > 4 }
+  calls.length > 4 ? false : true
 end
 
 def find_the_cheese(food)
-   cheese_types = ["cheddar", "gouda", "camembert"]
-    food.find {|cheese| cheese_types.include?(cheese)}
-end
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    food.find { |x| cheese_types.include?(x) ? x : nil}
+ end 
 
 
 
